1992e9b238184ad259905df5d1fe2399a80766b4
allenmurfee <allenmurfee123@gmail.com>
2022-10-21 09:41:30 -0400

    added inital questions

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const questions = [
+  {
+    type: "input",
+    message: "Enter team manager's name",
+    name: "manager",
+  },
+  {
+    type: "input",
+    message: "Enter team manager's ID",
+    name: "managerID",
+  },
+  {
+    type: "input",
+    message: "Enter team manager's email",
+    name: "managerEmail",
+  },
+  {
+    type: "input",
+    message: "Enter team manager's office number",
+    name: "managerOfficeNum",
+  },
+  {
+    type: "list",
+    message:
+      "Would you like to add engineer, intern, or finish building your team?",
+    name: "next",
+    choices: ["Add engineer", "Add intern", "Finish building my team"],
+  },
+];

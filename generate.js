05230b546acfc4d66aa81febf8c5cd99b8fea018
allenmurfee <allenmurfee123@gmail.com>
2022-10-22 14:13:06 -0400

    worked on start function

--- generate.js
+++ generate.js
@@ -1,40 +1,9 @@
 const index = require("./index");
 
-function generate(answers) {
-  return `<!DOCTYPE html>
-  <html lang="en">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  
-      <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi"
-        crossorigin="anonymous"
-      />
-      <title>Team Profiles</title>
-    </head>
-    <body>
-      <header>
-          <div class="jumbotron text-center">
-              <h1 class="display-4">Team Profiles</h1>
-              <p class="lead">Here's our team information!</p>
-            </div>
-      </header>
-      <script
-      src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"
-      integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-      crossorigin="anonymous"
-    ></script>
-    <script
-      src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"
-      integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct"
-      crossorigin="anonymous"
-    ></script>
-  </body>
-</html>`;
+function generateCards(answers) {
+  console.log(answers);
+   
 }
+
 
 module.exports = { generate };
--- index.js
+++ index.js
@@ -6,20 +6,54 @@ const intern = require("./lib/intern");
 const manager = require("./lib/manager");
 const generate = require("./generate");
 
+let questionAnswers = [];
+let firstQuestion = true;
+
 function start(questions) {
   inquirer.prompt(questions).then((answers) => {
-    if (answers.next === "Add engineer") {
+    if (firstQuestion) {
+      firstQuestion = false;
+      questionAnswers.push(
+        new Manager(
+          answers.managerName,
+          answers.managerID,
+          answers.managerEmail,
+          answers.managerOfficeNum
+        )
+      );
+      if (answers.next === "Add engineer") {
+        return start(engineer.engineerQuestions);
+      } else if (answers.next === "Add intern") {
+        return start(intern.internQuestions);
+      }
+    } else if (answers.next === "Add engineer") {
+      questionAnswers.push(
+        new Engineer(
+          answers.engineerName,
+          answers.engineerID,
+          answers.engineerEmail,
+          answers.engineerGitHub
+        )
+      );
       return start(engineer.engineerQuestions);
     } else if (answers.next === "Add intern") {
+      questionAnswers.push(
+        new Intern(
+          answers.internName,
+          answers.internID,
+          answers.internEmail,
+          answers.internSchool
+        )
+      );
       return start(intern.internQuestions);
     } else {
-      write("./dist/index.html", answers);
+      write("./dist/new.html", questionAnswers);
     }
   });
 }
 
 function write(fileName, answers) {
-  return fs.writeFile(fileName, generate(answers), (err) => {
+  return fs.writeFile(fileName, generate.generateCards(answers), (err) => {
     return err ? console.log("Error.") : console.log("Success!");
   });
 }

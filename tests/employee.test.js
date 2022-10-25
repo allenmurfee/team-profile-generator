4dc320965ea0d696b44e1eacb8f78b6c8cbcad6a
allenmurfee <allenmurfee123@gmail.com>
2022-10-25 15:26:55 -0400

    added some tests

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,12 +1,37 @@
 const Employee = require("../lib/employee");
 
 describe("Employee", () => {
-  it("should return the name of an employee", () => {
+  it("should return the name, ID, and email of an employee", () => {
     const testName = "Allen";
-    const inst = new Employee(testName);
+    const testId = "1";
+    const testEmail = "email@email.com";
+    const inst = new Employee(testName, testId, testEmail);
 
-    const result = inst.name;
+    const resultName = inst.name;
+    const resultId = inst.id;
+    const resultEmail = inst.email;
 
-    expect(result).toEqual(testName);
+    expect(resultName).toEqual(testName);
+    expect(resultId).toEqual(testId);
+    expect(resultEmail).toEqual(testEmail);
+  });
+  it("should return the name, ID, email, and role of an employee using getName(), getId(), getEmail(), and getRole()", () => {
+    const testName = "Allen";
+    const testId = "1";
+    const testEmail = "email@email.com";
+    const inst = new Employee(testName, testId, testEmail);
+
+    const resultName = inst.name;
+    const resultId = inst.id;
+    const resultEmail = inst.email;
+    const nameMethod = inst.getName();
+    const idMethod = inst.getId();
+    const emailMethod = inst.getEmail();
+    const roleMethod = inst.getRole();
+
+    expect(resultName).toEqual(nameMethod);
+    expect(resultId).toEqual(idMethod);
+    expect(resultEmail).toEqual(emailMethod);
+    expect("Employee").toEqual(roleMethod);
   });
 });
--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,12 +1,32 @@
 const engineer = require("../lib/engineer");
 
 describe("Engineer", () => {
-    it("should return the name of an engineer", () => {
-      const testName = "Allen";
-      const inst = new engineer.Engineer(testName);
-  
-      const result = inst.name;
-  
-      expect(result).toEqual(testName);
-    });
+  it("should return the name, ID, email, and GitHub of an engineer", () => {
+    const testName = "Allen";
+    const testId = "1";
+    const testEmail = "email@email.com";
+    const testGithub = "username";
+    const inst = new engineer.Engineer(testName, testId, testEmail, testGithub);
+
+    const resultName = inst.name;
+    const resultId = inst.id;
+    const resultEmail = inst.email;
+    const resultGithub = inst.engineerGitHub;
+
+    expect(resultName).toEqual(testName);
+    expect(resultId).toEqual(testId);
+    expect(resultEmail).toEqual(testEmail);
+    expect(resultGithub).toEqual(testGithub);
   });
+  it("should return the GitHub and role of an engineer using getGithub() and getRole()", () => {
+    const testGithub = "username";
+    const inst = new engineer.Engineer("", "", "", testGithub);
+
+    const resultGithub = inst.engineerGitHub;
+    const githubMethod = inst.getGithub();
+    const roleMethod = inst.getRole();
+
+    expect(resultGithub).toEqual(githubMethod);
+    expect("Engineer").toEqual(roleMethod);
+  });
+});

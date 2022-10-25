615af26216c37e38a542b5b121d4cc6760c8cfc1
allenmurfee <allenmurfee123@gmail.com>
2022-10-25 15:35:23 -0400

    tests all working

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,12 +1,33 @@
 const intern = require("../lib/intern");
 
-describe("Engineer", () => {
-    it("should return the name of a intern", () => {
-      const testName = "Allen";
-      const inst = new intern.Intern(testName);
-  
-      const result = inst.name;
-  
-      expect(result).toEqual(testName);
-    });
+describe("Intern", () => {
+  it("should return the name, ID, email, and school of an intern", () => {
+    const testName = "Allen";
+    const testId = "1";
+    const testEmail = "email@email.com";
+    const testSchool = "school";
+    const inst = new intern.Intern(testName, testId, testEmail, testSchool);
+
+    const resultName = inst.name;
+    const resultId = inst.id;
+    const resultEmail = inst.email;
+    const resultSchool = inst.internSchool;
+
+    expect(resultName).toEqual(testName);
+    expect(resultId).toEqual(testId);
+    expect(resultEmail).toEqual(testEmail);
+    expect(resultSchool).toEqual(testSchool);
   });
+  it("should return the school and role of an intern using getSchool() and getRole()", () => {
+    const testSchool = "school";
+    const inst = new intern.Intern("", "", "", testSchool);
+
+    const resultSchool = inst.internSchool;
+
+    const schoolMethod = inst.getSchool();
+    const roleMethod = inst.getRole();
+
+    expect(resultSchool).toEqual(schoolMethod);
+    expect("Intern").toEqual(roleMethod);
+  });
+});
--- tests/manager.test.js
+++ tests/manager.test.js
@@ -1,12 +1,28 @@
 const manager = require("../lib/manager");
 
-describe("Engineer", () => {
-    it("should return the name of a manager", () => {
-      const testName = "Allen";
-      const inst = new manager.Manager(testName);
-  
-      const result = inst.name;
-  
-      expect(result).toEqual(testName);
-    });
+describe("Manager", () => {
+    it("should return the name, ID, email, and office number of an manager", () => {
+        const testName = "Allen";
+        const testId = "1";
+        const testEmail = "email@email.com";
+        const testOffice = "2";
+        const inst = new manager.Manager(testName, testId, testEmail, testOffice);
+    
+        const resultName = inst.name;
+        const resultId = inst.id;
+        const resultEmail = inst.email;
+        const resultOffice = inst.managerOfficeNum;
+    
+        expect(resultName).toEqual(testName);
+        expect(resultId).toEqual(testId);
+        expect(resultEmail).toEqual(testEmail);
+        expect(resultOffice).toEqual(testOffice);
+      });
+      it("should return the role of a manager using getRole()", () => {
+        const inst = new manager.Manager("", "", "", "");
+
+        const roleMethod = inst.getRole();
+
+        expect("Manager").toEqual(roleMethod);
+      });
   });
